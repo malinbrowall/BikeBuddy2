40a5284e89ba1bfad77509443de87d3f5b139da8
Dennis <aqwees@gmail.com>
2016-05-03 14:06:57 +0200

    add event

---
+++ events.js
@@ -0,0 +1,24 @@
+
+    var config = require('./config.json'), //config file contains all tokens and other private info
+    db = require('orchestrate')(config.db); //config.db holds Orchestrate token
+
+
+exports.getEvent = function(req, res) {
+
+  
+
+  db.newSearchBuilder()
+    .collection('Event')
+    .limit(10)
+    .query('*')
+    .then(function (topics){
+      res.render('home', { title: 'Express', topics: topics.body.results, totalCount: topics.body.total_count});
+    });
+};
+
+
+exports.addEvent = function() {
+  db.post('Event',{
+    "title": "Test"
+  });
+};
